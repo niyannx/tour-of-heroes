4ab7bfb3c4e8e8d78dde0db9cace5bd904a40212
Niya Nicolova <niyannx@gmail.com>
2023-01-21 14:35:20 +0200

    Added Hero Details component functionality

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
