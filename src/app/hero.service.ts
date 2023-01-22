544bc306f28b51648ee7e2eb3a13f3d7fc871baa
Niya Nicolova <niyannx@gmail.com>
2023-01-22 14:37:09 +0200

    Injected HeroService and added functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,16 +3,22 @@ import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes'
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  // This is an example of a typical service-in-service scenario:
+  // The MessageService is injected into the HeroService which is injected into the HeroesComponent
+  constructor(private messageService: MessageService) { }
 
   getHeroes() : Observable<Hero[]> {
     // of(HEROES) returns an Observable<Hero[]>
-    return of(HEROES);
+    const heroes = of(HEROES);
+
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
   }
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent {
+  // messageService property must be public because we're going to bind to it in the template
+  constructor(public messageService: MessageService) {
 
+  }
 }
