7a64c7f17367544b72ba3662ef5a975f298b7501
Niya Nicolova <niyannx@gmail.com>
2023-01-22 16:15:37 +0200

    Enabled HTTP services and simulated a data server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms'; // <- NgModule
 import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
 
+import { InMemoryDataService } from './in-memory-data.service';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
